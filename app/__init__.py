a9ba8ab05eaa44ee27fb68869f551637fc7c6735
emmanuelkaribiye <emmanuelkaribiye31@gmail.com>
2025-06-20 22:28:41 +0100

    pr changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import os
+import json
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+
 
 load_dotenv()
 app = Flask(__name__)
@@ -8,55 +10,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    experiences = [
-        {
-            "role": "Software Engineering Intern",
-            "company": "JP Morgan Chase & Co.",
-            "duration": "June 2024 – August 2024",
-            "details": [
-                "Developed a back-end application using Java Spring Boot, Terraform, Docker, and AWS to perform data validation and quality checks.",
-                "Created a RESTful API with MySQL to store and manage data.",
-                "Resolved critical data quality bugs, helping protect the security of over 80 million clients globally."
-            ]
-        },
-        {
-            "role": "Data Analyst Intern",
-            "company": "Wuxi Biologics",
-            "duration": "January 2023 – September 2023",
-            "details": [
-                "Performed analytics and visualizations on large datasets.",
-                "Automated dashboard generation using R, Python, SQL, and Tableau, improving team efficiency.",
-                "Resolved key Microsoft SQL issues involving over 50,000 rows of data."
-            ]
-        },
-        {
-            "role": "Mechanical Engineering Intern",
-            "company": "Silent Aire",
-            "duration": "July 2022 - September 2022",
-            "details": [
-                "Carrying out pressure/leak analysis on the doors and coming up with a design solution hence boosting its efficiency.",
-                "Designing plans and specifying dimensions to be followed in building HVAC systems for data centers.",
-                "Observing and documenting the Standard operating procedures for each role involved in building our hyperscale ventilation systems."
-            ]
-        }
+
+    try:
+        experiences = json.loads(os.getenv("EXPERIENCE_JSON", "[]"))
+        education= json.loads(os.getenv("EDUCATION_JSON", "[]"))
+        hobbies= json.loads(os.getenv("HOBBIES_JSON", "[]"))
+    except json.JSONDecodeError as e:
+        print("Error parsing EXPERIENCE_JSON:", e)
+        experiences = []
+        education = []
+        hobbies = []
 
 
-    ]
-
-    education = [
-        {
-            "degree": "BSc Mechanical Engineering",
-            "institution": "Dublin City University",
-            "duration": "2021 – 2025",
-            "details": [
-                "Class rep for two years"
-            ]
-        }
-
-    ]
-
-    hobbies = [
-        "I love to listen to Music, I also play the piano and guitar",
-        "I love Gaming, I play games like Fifa and Call of Duty all the time"
-    ]    
     return render_template('index.html', title="MLH Fellow", experiences=experiences, education=education, hobbies=hobbies, url=os.getenv("URL"))
